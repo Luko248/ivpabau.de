20b6ad94554f999cc5a246f5e492b1ad5526d4cf
Lukáš Chylík <lukas.chylik@riganti.cz>
2023-01-22 16:23:00 +0100

    header styled

--- Resources/Scripts/nav.ts
+++ Resources/Scripts/nav.ts
@@ -1,14 +1,12 @@
 const body = document.body;
-const headerLanding = document.querySelector('.header--landing');
-const nav = document.querySelector('.nav');
-const menu = document.querySelector('.nav__menu')
-const menuBtn = document.querySelector('.burger-button')
+const menu = document.querySelector('.menu')
+const menuBtn = document.getElementById('menuBtn')
 const navItem = document.querySelectorAll('.nav__item')
 
 export function navInit(){
     menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('burger-button--open')
-        menu.classList.toggle('nav__menu--open')
+        menuBtn.classList.toggle('burger--open')
+        menu.classList.toggle('menu--open')
         body.classList.toggle('overflow-hidden')
     })
     
@@ -20,9 +18,9 @@ export function navInit(){
 
             item.classList.add('nav__item--active')
 
-            if (menu.classList.contains('nav__menu--open')){
-                menuBtn.classList.remove('burger-button--open')
-                menu.classList.remove('nav__menu--open')
+            if (menu.classList.contains('menu--open')){
+                menuBtn.classList.remove('burger--open')
+                menu.classList.remove('menu--open')
                 body.classList.remove('overflow-hidden')
             }
         })
@@ -30,8 +28,8 @@ export function navInit(){
 
     document.addEventListener('click', (e) => {
         if (e.target !== menuBtn){
-            menuBtn.classList.remove('burger-button--open')
-            menu.classList.remove('nav__menu--open')
+            menuBtn.classList.remove('burger--open')
+            menu.classList.remove('menu--open')
             body.classList.remove('overflow-hidden')
         }
     })
